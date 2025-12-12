fffade14255d6ca26926e595162d642a3aafbb9b
Delina Mulubrhan <delinamulubrhann@gmail.com>
2025-12-12 23:20:18 +0300

    Update Azop_Academy project with all recent changes

--- backend-mongoose/controllers/teacherController.js
+++ backend-mongoose/controllers/teacherController.js
@@ -1,48 +1,60 @@
 const Teacher = require("../models/Teacher");
 
-// CREATE TEACHER
-exports.createTeacher = async (req, res) => {
-  try {
-    const { name, subject, experience } = req.body;
+//CREATE TEACHER
+exports.createTeacher = async(req,res)=>{
+  try{
+    const{name, subject, experience}= req.body;
+    const photo = req.file? req.file.filename: ""
 
-    const photo = req.file ? req.file.filename : "";
-
-    const newTeacher = new Teacher({
-      name,
-      subject,
-      experience,
-      photo,
-    });
-
-    await newTeacher.save();
-
+  const newTeacher = new Teacher({
+    name,
+    subject,
+    experience,
+    photo
+  });
+  await newTeacher.save()
     res.status(201).json(newTeacher);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
+  }
+  catch(error){
+    res.status(400).json({message: error.message})
   }
 };
 
-// GET ALL TEACHERS
-exports.getTeachers = async (req, res) => {
-  try {
+exports.getAllTeachers = async(req,res)=>{
+  try{
     const teachers = await Teacher.find();
-    res.status(200).json(teachers);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(200).json(teachers)
   }
+  catch(error){
+    res.status(500).json({message:error.message})
+  }
+}
+
+exports.getTeachersById = async(req,res)=>{
+  try{
+    const teacher = await Teacher.findById(req.param.id)
+    res.status(200).json(teacher)
+  }
+  catch(error){
+    res.status(500).json({message: "Teacher not found"})
+  }
+  if (!teacher) return res.status(404).json({ message: "Teacher not found" });
+
 };
 
-// GET ONE TEACHER
-exports.getTeacherById = async (req, res) => {
-  try {
-    const teacher = await Teacher.findById(req.params.id);
-    if (!teacher) return res.status(404).json({ message: "Teacher not found" });
 
-    res.status(200).json(teacher);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+
+
+
+
+
+
+
+
+
+
+
+
 
 // UPDATE TEACHER
 exports.updateTeacher = async (req, res) => {
